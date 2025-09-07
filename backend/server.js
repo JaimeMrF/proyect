5bb9ad0bfe27f4b542cc114f5c3cbe2f79af77d7
Jaime Alejandro Vega Barbosa <jaime.ja203@gmail.com>
2025-09-07 11:32:43 -0500

    Proyecto desde 0

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,23 @@
 require("dotenv").config();
-
+const cors = require("cors");
 const path = require("path");
 const express = require("express");
+const mongoose  = require("mongoose");
+const router = require(path.join(__dirname, "/routes/tareas.js"));
 const app = express();
 const PORT = process.env.PORT;
 
+app.use(cors({
+  origin: ["http://localhost:5500", "http://127.0.0.1:5500"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(console.log("DB conectada"))
+    .catch(err => console.log(err))
+
+app.use("/tareas", router);
 
 app.use(express.static(path.join(__dirname, "../frontend")));
 
@@ -15,3 +28,4 @@ app.get("/", (req, res) => {
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
+
