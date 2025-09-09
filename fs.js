07d4b17c3f15b3f807dd99969a742c204acb5219
Jaime Alejandro Vega Barbosa <jaime.ja203@gmail.com>
2025-09-08 23:26:51 -0500

    writefilesync

--- fs.js
+++ fs.js
@@ -1,6 +1,12 @@
 const fs = require('fs');
 
 const first = fs.readFileSync("./data/first.txt", "utf-8");
+const second = fs.readFileSync("./data/second.txt", "utf-8");
 
 console.log(first);
+console.log(second);
+
+const title = 'contenido del archivo';
+
+fs.writeFileSync("./data/fourth.txt", title);
  
