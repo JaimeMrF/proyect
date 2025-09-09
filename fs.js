482259c83854a527e01ba463527f23d5c6ac4891
Jaime Alejandro Vega Barbosa <jaime.ja203@gmail.com>
2025-09-08 23:44:19 -0500

    si

--- fs.js
+++ fs.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+const fs = require("fs");
 
 // const first = fs.readFileSync("./data/first.txt", "utf-8");
 // const second = fs.readFileSync("./data/second.txt", "utf-8");
@@ -11,28 +11,27 @@ const fs = require('fs');
 // fs.writeFileSync("./data/fourth.txt", title, {
 //     flag: 'a'
 // });
- 
-fs.readFile('./data/first.txt', 'utf-8', (error, data) => {
-    if(error) {
-        console.log(error)
+
+fs.readFile("./data/first.txt", "utf-8", (error, data) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(data);
+  }
+
+  fs.readFile("./data/second.txt", "utf-8", (error, data) => {
+    if (error) {
+      console.log(error);
     } else {
-        console.log(data)
+      console.log(data);
     }
+  });
 
-    fs.readFile('./data/second.txt', 'utf-8', (error, data) => {
-        if(error) {
-        console.log(error)
+  fs.writeFile("./data/newfile.txt", "archivo creado", (err, data) => {
+    if (err) {
+      console.log(err);
     } else {
-        console.log(data)
-        }
-    })
-
-    fs.writeFile('./data/newfile.txt', 'archivo creado', (err, data)=> {
-        if (err) {
-            console.log(err)
-        } else {
-            console.log(data)
-        }
-    })
-})  
-
+      console.log(data);
+    }
+  });
+});
