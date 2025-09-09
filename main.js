bd3529f0de2064c418e53b5ec2e3f98119a12824
Jaime Alejandro Vega Barbosa <jaime.ja203@gmail.com>
2025-09-08 23:00:22 -0500

    modulo math creado

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
-const {user, mayor} = require("./module/myModule");
+// const {user, mayor} = require("./module/myModule");
 
-console.log(`${user} es mayor?: ${mayor}`);
+// console.log(`${user} es mayor?: ${mayor}`);
+
+const math = require("./math/index");
+
+console.log(
+    math.suma(4,5),
+    math.resta(4,5),
+    math.division(4,5),
+    math.multiplicacion(4,5)
+)
---
+++ math/index.js
@@ -0,0 +1,22 @@
+function suma (x,y) {
+    return x+y;
+}
+
+function resta (x,y) {
+    return x-y;
+}
+
+function division (x,y) {
+    return x/y;
+}
+
+function multiplicacion (x,y) {
+    return x*y;
+}
+
+module.exports = {
+    suma,
+    resta,
+    division,
+    multiplicacion
+}
