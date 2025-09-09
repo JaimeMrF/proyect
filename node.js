ae4ae6b662e4e06e1e65564e5746c5d10a1e1478
Jaime Alejandro Vega Barbosa <jaime.ja203@gmail.com>
2025-09-08 22:14:33 -0500

    funciones normales js

--- node.js
+++ node.js
@@ -2,19 +2,33 @@
 console.log(`--------\nHello NodeJS\n--------\n`);
 //---------------------------------//
 
-let username = 'Jaime';
+// let username = 'Jaime';
 
-let age = 18;
+// let age = 18;
 
-let hasHobbies = true;
+// let hasHobbies = true;
 
-let streak = [10,20,30];
+// let streak = [10,20,30];
 
-let user = {
-    name: 'Jaime',
-    lastname: 'Vega'
-}
+// let user = {
+//     name: 'Jaime',
+//     lastname: 'Vega'
+// }
 
-const pi = 3.1415
+// const pi = 3.1415
 
-console.log(`username: ${username}\nage: ${age}\nTiene hobbies: ${hasHobbies}\nRacha: ${streak}\nUsuario: ${JSON.stringify(user, null, 2)}\nPi: ${pi}`);
+// console.log(
+//     `username: ${username}\n
+//     age: ${age}\n
+//     Tiene hobbies: ${hasHobbies}\n
+//     Racha: ${streak}\n
+//     Usuario: ${JSON.stringify(user, null, 2)}\n
+//     Pi: ${pi}`
+// );
+
+// function showUserInfo(userName, ageUser) {
+//     return `The user is ${userName}, the user have ${ageUser} years old`
+// };
+
+// console.log(showUserInfo('Jaime', 18));
+// console.log(showUserInfo('Valeria', 16));
